51f03301e0af44a93589c1e9034ff694f8caadca
maraero <themaraero@gmail.com>
2023-01-22 18:06:03 +0300

    update tests for params

--- internal/resizesrv/resizesrv_test.go
+++ internal/resizesrv/resizesrv_test.go
@@ -20,48 +20,73 @@ func TestExtractParams(t *testing.T) {
 			})
 		}
 	})
+
 	//nolint:lll
-	validURL := "raw.githubusercontent.com/OtusGolang/final_project/master/examples/image-previewer/_gopher_original_1024x504.jpg"
+	testURL := "http://raw.githubusercontent.com/OtusGolang/final_project/master/examples/image-previewer/_gopher_original_1024x504.jpg"
 
 	t.Run("valid params", func(t *testing.T) {
-		ip, err := rs.ExtractParams("300/200/" + validURL)
+		ip, err := rs.ExtractParams("300/200/" + testURL)
 		require.NoError(t, err)
-		require.Equal(t, &ImageParams{Width: 300, Height: 200, URL: validURL}, ip)
+		require.Equal(t, &ImageParams{Width: 300, Height: 200, URL: testURL}, ip)
 	})
 
-	t.Run("width is not number", func(t *testing.T) {
-		_, err := rs.ExtractParams("a/100/" + validURL)
-		require.Error(t, err)
-		require.ErrorContains(t, err, "width must be int")
+	t.Run("width validation", func(t *testing.T) {
+		tests := []struct {
+			input         string
+			expectedError string
+		}{
+			{input: "a/100/" + testURL, expectedError: "width must be int"},
+			{input: "34.34/100/" + testURL, expectedError: "width must be int"},
+			{input: "-5/100/" + testURL, expectedError: "width must be greater than zero"},
+		}
+
+		for _, tc := range tests {
+			tc := tc
+			t.Run(tc.input, func(t *testing.T) {
+				_, err := rs.ExtractParams(tc.input)
+				require.Error(t, err)
+				require.ErrorContains(t, err, tc.expectedError)
+			})
+		}
 	})
 
-	t.Run("width is float number", func(t *testing.T) {
-		_, err := rs.ExtractParams("34.34/100/" + validURL)
-		require.Error(t, err)
-		require.ErrorContains(t, err, "width must be int")
+	t.Run("height validation", func(t *testing.T) {
+		tests := []struct {
+			input         string
+			expectedError string
+		}{
+			{input: "200/a/" + testURL, expectedError: "height must be int"},
+			{input: "200/4.23/" + testURL, expectedError: "height must be int"},
+			{input: "200/-100/" + testURL, expectedError: "height must be greater than zero"},
+		}
+
+		for _, tc := range tests {
+			tc := tc
+			t.Run(tc.input, func(t *testing.T) {
+				_, err := rs.ExtractParams(tc.input)
+				require.Error(t, err)
+				require.ErrorContains(t, err, tc.expectedError)
+			})
+		}
 	})
 
-	t.Run("width is negative", func(t *testing.T) {
-		_, err := rs.ExtractParams("-5/100/" + validURL)
-		require.Error(t, err)
-		require.ErrorContains(t, err, "width must be greater than zero")
-	})
+	t.Run("adds schema to URLs if it is required", func(t *testing.T) {
+		tests := []struct {
+			input    string
+			expected string
+		}{
+			{input: "test.com", expected: "http://test.com"},
+			{input: "http://test.com", expected: "http://test.com"},
+			{input: "https://test.com", expected: "https://test.com"},
+		}
 
-	t.Run("height is not number", func(t *testing.T) {
-		_, err := rs.ExtractParams("200/a/" + validURL)
-		require.Error(t, err)
-		require.ErrorContains(t, err, "height must be int")
-	})
-
-	t.Run("height is float number", func(t *testing.T) {
-		_, err := rs.ExtractParams("200/4.23/" + validURL)
-		require.Error(t, err)
-		require.ErrorContains(t, err, "height must be int")
-	})
-
-	t.Run("height is negative", func(t *testing.T) {
-		_, err := rs.ExtractParams("200/-100/" + validURL)
-		require.Error(t, err)
-		require.ErrorContains(t, err, "height must be greater than zero")
+		for _, tc := range tests {
+			tc := tc
+			t.Run(tc.input, func(t *testing.T) {
+				result, err := rs.ExtractParams("200/100/" + tc.input)
+				require.NoError(t, err)
+				require.Equal(t, tc.expected, result.URL)
+			})
+		}
 	})
 }
