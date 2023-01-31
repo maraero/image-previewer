43d6096b78ff5d0a418cd6b9b1ed9997a12211e8
maraero <themaraero@gmail.com>
2023-01-31 22:20:22 +0300

    move downloadJPEG to separate file

--- internal/imagesrv/image_srv.go
+++ internal/imagesrv/image_srv.go
@@ -3,10 +3,8 @@ package imagesrv
 import (
 	"bytes"
 	"context"
-	"fmt"
 	"image"
 	"image/jpeg"
-	"io"
 	"net/http"
 
 	"github.com/disintegration/imaging"
@@ -37,7 +35,7 @@ func (is *ImageSrv) GetResizedImg(params string, reqHeaders http.Header) ([]byte
 		return nil, err
 	}
 
-	img, err := is.getImg(imgParams.URL, reqHeaders)
+	img, err := is.downloadJPEG(imgParams.URL, reqHeaders)
 	if err != nil {
 		is.logger.Error(err)
 		return nil, err
@@ -58,25 +56,6 @@ func (is *ImageSrv) GetResizedImg(params string, reqHeaders http.Header) ([]byte
 	}
 
 	return imgBytes, nil
-}
-
-func (is *ImageSrv) getImg(url string, reqHeaders http.Header) (*image.Image, error) {
-	file, err := is.downloadFile(url, reqHeaders)
-	if err != nil {
-		return nil, err
-	}
-
-	jpeg := is.isFileJPEG(file[0:3])
-	if !jpeg {
-		return nil, fmt.Errorf("%s %w", url, ErrIsNotJPEG)
-	}
-
-	img, _, err := image.Decode(bytes.NewReader(file))
-	if err != nil {
-		return nil, ErrCanNotDecodeJPEG
-	}
-
-	return &img, nil
 }
 
 func (is *ImageSrv) encodeImageToBytes(img *image.Image) ([]byte, error) {
@@ -103,39 +82,6 @@ func (is *ImageSrv) isJPEG(respHeaders http.Header) bool {
 
 func (is *ImageSrv) resizeImage(img *image.Image, width, height int) image.Image {
 	return imaging.Fill(*img, width, height, imaging.Center, imaging.Lanczos)
-}
-
-func (is *ImageSrv) downloadFile(url string, headers http.Header) ([]byte, error) {
-	req, err := http.NewRequestWithContext(is.cancelContext, http.MethodGet, url, nil)
-	if err != nil {
-		return nil, fmt.Errorf("%w: %s", ErrCanNotBuildRequest, err)
-	}
-
-	for h := range headers {
-		req.Header.Add(h, headers.Get(h))
-	}
-
-	resp, err := is.httpClient.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("%w: %s", ErrCanNotMakeRequest, err)
-	}
-
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("%w from %s", ErrCanNotDownloadFile, url)
-	}
-
-	respHeaders := resp.Header.Clone()
-	if !is.isJPEG(respHeaders) {
-		return nil, ErrIsNotJPEG
-	}
-
-	defer resp.Body.Close()
-	image, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("%w: %s", ErrCanNotReadResponseBody, err)
-	}
-
-	return image, nil
 }
 
 func (is *ImageSrv) isFileJPEG(firstFileBytes []byte) bool {
---
+++ internal/imagesrv/loader.go
@@ -0,0 +1,53 @@
+package imagesrv
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	_ "image/jpeg"
+	"io"
+	"net/http"
+)
+
+func (is *ImageSrv) downloadJPEG(url string, headers http.Header) (*image.Image, error) {
+	req, err := http.NewRequestWithContext(is.cancelContext, http.MethodGet, url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %s", ErrCanNotBuildRequest, err)
+	}
+
+	for h := range headers {
+		req.Header.Add(h, headers.Get(h))
+	}
+
+	resp, err := is.httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %s", ErrCanNotMakeRequest, err)
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("%w from %s", ErrCanNotDownloadFile, url)
+	}
+
+	respHeaders := resp.Header.Clone()
+	if !is.isJPEG(respHeaders) {
+		return nil, ErrIsNotJPEG
+	}
+
+	defer resp.Body.Close()
+	file, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("%w: %s", ErrCanNotReadResponseBody, err)
+	}
+
+	jpeg := is.isFileJPEG(file[0:3])
+	if !jpeg {
+		return nil, ErrIsNotJPEG
+	}
+
+	img, _, err := image.Decode(bytes.NewReader(file))
+	if err != nil {
+		return nil, ErrCanNotDecodeJPEG
+	}
+
+	return &img, nil
+}
